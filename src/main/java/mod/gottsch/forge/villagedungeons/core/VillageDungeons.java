0d3e7430d58db051cb60d9cbd5e50176fa62577b
gottsch <17928819+gottsch@users.noreply.github.com>
2025-03-24 22:38:01 -0400

    initial setup commit.

---
+++ src/main/java/mod/gottsch/forge/villagedungeons/core/VillageDungeons.java
@@ -0,0 +1,16 @@
+package mod.gottsch.forge.villagedungeons.core;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * @author Mark Gottschling on 3/24/2025
+ */
+@Mod(value = VillageDungeons.MOD_ID)
+public class VillageDungeons {
+    private static final String MOD_ID = "villagedungeons";
+
+    public VillageDungeons() {
+
+    }
+
+}
---
+++ src/main/java/mod/gottsch/forge/villagedungeons/core/config/Config.java
@@ -0,0 +1,7 @@
+package mod.gottsch.forge.villagedungeons.core.config;
+
+/**
+ * Created by Mark Gottschling on 3/24/2025
+ */
+public class Config {
+}
